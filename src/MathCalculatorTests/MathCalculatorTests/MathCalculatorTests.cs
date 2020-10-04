f68bee118893be181ced6a7352a90c4e7b3e02c3
dmitr2102 <dmitr2102@gmail.com>
2020-10-04 12:42:00 +0300

    changes 2

---
+++ src/MathCalculatorTests/MathCalculatorTests/MathCalculatorTests.cs
@@ -0,0 +1,18 @@
+using System;
+using Xunit;
+using MathCalculator;
+
+namespace MathCalculatorTests
+{
+    public class MathCalculatorTests
+    {
+        [Fact]
+        public void WhenSumFiveAndThreeThenGotEight()
+        {
+            //Arrange
+            const int Five = 5;
+            const int Three = 3;
+            MathCalculator calculator = new MathCalculator();
+        }
+    }
+}
