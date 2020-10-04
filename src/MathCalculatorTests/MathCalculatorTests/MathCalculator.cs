188b46d0b59aafef8e3b441322f0e1f0d30207cf
dmitr2102 <dmitr2102@gmail.com>
2020-10-04 12:49:10 +0300

    first test

---
+++ src/MathCalculatorTests/MathCalculatorTests/MathCalculator.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace MathCalculatorTests
+{
+    public class MathCalculator
+    {
+    }
+}
--- src/MathCalculatorTests/MathCalculatorTests/MathCalculatorTests.cs
+++ src/MathCalculatorTests/MathCalculatorTests/MathCalculatorTests.cs
@@ -1,6 +1,5 @@
 using System;
 using Xunit;
-using MathCalculator;
 
 namespace MathCalculatorTests
 {
