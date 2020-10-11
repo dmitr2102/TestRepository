b2ee162720ef6da7d6d4ae50da9a44a87b9ff84b
aleokos <levpanin@gmail.com>
2020-10-11 12:13:38 +0300

    eas

--- PullRequestTraining/TestRepository-master/src/MathCalculator/MathCalculator/MathCalculator.cs
+++ PullRequestTraining/TestRepository-master/src/MathCalculator/MathCalculator/MathCalculator.cs
@@ -2,8 +2,9 @@ namespace MathCalculatorTests
 {
     public class MathCalculator
     {
-        public int Sum(int a, int b){
-            return a+b;
+        public int Sum(string a, string b)
+        {
+            return(Int(a) + Int(b));
         }
 
         public int Sub(int a, int b)
