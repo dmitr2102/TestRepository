cdf2cbcb42984089464da4305561db711a12d7e4
dmitr2102 <dmitr2102@gmail.com>
2020-10-18 12:52:06 +0300

    Add Paper1 test

--- src/RockScissorsPaperTests/WinnerCheckerTest.cs
+++ src/RockScissorsPaperTests/WinnerCheckerTest.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using System;
 using Xunit;
 using RockScissorsPaperLib;
@@ -9,11 +10,19 @@ namespace RockScissorsPaperTests
         private WinnerChecker checker = new WinnerChecker();
         // Random rnd = new Random();
         const string rock = "rock";
+        const string paper = "paper";
+        const string scissors = "scissors";
         [Fact]
         public void Rock1()
         {
             var res = checker.Game(rock, 0);
             Assert.Equal(res, "You win");
+        }
+
+        [Fact]
+        public void Paper1(){
+            var res = checker.Game("paper", 1);
+            Assert.Equal(res, "You lose");
         }
     }
 }
