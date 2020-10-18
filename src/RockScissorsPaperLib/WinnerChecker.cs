e0b990f9403b0f44862d181d5e1974f4964527e0
dmitr2102 <dmitr2102@gmail.com>
2020-10-18 14:04:20 +0300

    Remade to switch

--- src/RockScissorsPaperLib/WinnerChecker.cs
+++ src/RockScissorsPaperLib/WinnerChecker.cs
@@ -1,17 +1,36 @@
 ﻿using System.Security.Cryptography;
 using System;
-// using RandomNumberGenerator;
 
 namespace RockScissorsPaperLib
 {
     public class WinnerChecker
     {
-        public string Game(string s, int n)
+        enum t {paper = 0, scissors = 1, rock = 2, lizard}
+        public string Game(int p, int n)
         {
-            Random rnd = new Random();
             //0 - бумага, 1 - ножницы, 2 - камень
-            // var n = rnd.Next(0, 3);
-            if (s == "rock"){
+            switch (p, n){
+                case (0, 0): 
+                case (1, 1):
+                case (2, 2):
+                    return "Draw";
+                    break;
+                case (0, 1):
+                case (1, 2):
+                case (2, 0):
+                    return "You lose";
+                    break;
+                case (0, 2):
+                case (1, 0):
+                case (2, 1):
+                    return "You win";
+                    break;
+                default:
+                    return "Error";
+                    break;
+            }
+
+            /*if (s == "rock"){
                 if (n == 0)
                 {
                     return "You lose";
@@ -54,7 +73,7 @@ namespace RockScissorsPaperLib
                 else return "You lose";
             }
 
-            else return "Error";
+            else return "Error";*/
         }
     }
 }
