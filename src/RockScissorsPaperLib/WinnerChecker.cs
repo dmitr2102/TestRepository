9b4af4a81156f83bcdf2edd323372659d3a37247
dmitr2102 <dmitr2102@gmail.com>
2020-10-18 12:20:05 +0300

    Add void WinnerChecker

--- src/RockScissorsPaperLib/WinnerChecker.cs
+++ src/RockScissorsPaperLib/WinnerChecker.cs
@@ -1,9 +1,27 @@
-﻿using System;
+﻿using System.Security.Cryptography;
+using System;
+using RandomNumberGenerator;
 
 namespace RockScissorsPaperLib
 {
     public class WinnerChecker
     {
-        
+        string Game(string obj)
+        {
+            Random rnd = new Random();
+            //1 - бумага, 2 - ножницы, 3 - камень
+            var n = rnd.Next(1, 4);
+            if (obj == "камень"){
+                if (n == 1){
+                    return "You lose";
+                }
+                else if (n == 2){
+                    return "You win";
+                }
+                else{
+                    return "Draw";
+                }
+            }
+        }
     }
 }
