1d826bbe32694ec8cdd3ea38448a649619cb9277
Egor <o.grecky@outlook.com>
2020-10-18 12:59:31 +0300

    Added 2  Rock tests

--- src/RockScissorsPaperTests/WinnerCheckerTest.cs
+++ src/RockScissorsPaperTests/WinnerCheckerTest.cs
@@ -9,6 +9,9 @@ namespace RockScissorsPaperTests
     {
         private WinnerChecker checker = new WinnerChecker();
         // Random rnd = new Random();
+        //0 == paper
+        //1 == scissors
+        //2 == rock
         const string rock = "rock";
         const string paper = "paper";
         const string scissors = "scissors";
@@ -16,13 +19,28 @@ namespace RockScissorsPaperTests
         public void Rock1()
         {
             var res = checker.Game(rock, 0);
-            Assert.Equal(res, "You win");
+            Assert.Equal("You lose", res);
+        }
+        [Fact]
+        public void Rock2()
+        {
+            var res = checker.Game(rock, 1);
+            Assert.Equal("You win", res);
+        }
+        [Fact]
+        public void Rock3()
+        {
+            var res = checker.Game(rock, 2);
+            Assert.Equal("Draw", res);
         }
 
+
+
         [Fact]
-        public void Paper1(){
-            var res = checker.Game("paper", 1);
-            Assert.Equal(res, "You lose");
+        public void Paper1()
+        {
+            var res = checker.Game(paper, 1);
+            Assert.Equal("You lose", res);
         }
     }
 }
