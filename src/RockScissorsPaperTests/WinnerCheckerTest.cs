26658c2ac508a34b565792f6068ad4835ed60cf2
dmitr2102 <dmitr2102@gmail.com>
2020-10-25 12:14:19 +0300

    TheoryTest

--- src/RockScissorsPaperTests/WinnerCheckerTest.cs
+++ src/RockScissorsPaperTests/WinnerCheckerTest.cs
@@ -13,7 +13,7 @@ namespace RockScissorsPaperTests
         //2 == rock
         enum t {paper = 0, scissors = 1, rock = 2, lizard}
 
-        [Fact]
+/*        [Fact]
         public void Rock1()
         {
             var res = checker.Game((int)t.rock, (int)t.paper);
@@ -72,6 +72,52 @@ namespace RockScissorsPaperTests
         {
             var res = checker.Game((int)t.scissors, (int)t.rock);
             Assert.Equal("You lose", res);
+        }
+*/
+        /*[Fact]
+        public void WinnerCheckerAllTest()
+        {
+            CheckTest(1, 2, "Win");
+        }
+
+        public void  CheckTest(int player1, int player2, string expectedResult){
+            WinnerChecker winnerChecker = new WinnerChecker();
+            var result = winnerChecker.Game(player1, player2);
+
+            Assert.Equal(expectedResult, result);
+        }*/
+
+        [Theory]
+        [InlineData((int)t.paper, (int)t.paper, "Draw")]
+        [InlineData((int)t.paper, (int)t.scissors, "You lose")]
+        [InlineData((int)t.paper, (int)t.rock, "You win")]
+        public void  CheckPaperTestTheory(int player1, int player2, string expectedResult){
+            WinnerChecker winnerChecker = new WinnerChecker();
+            var result = winnerChecker.Game(player1, player2);
+
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [InlineData((int)t.rock, (int)t.paper, "You lose")]
+        [InlineData((int)t.rock, (int)t.scissors, "You win")]
+        [InlineData((int)t.rock, (int)t.rock, "Draw")]
+        public void  CheckStoneTestTheory(int player1, int player2, string expectedResult){
+            WinnerChecker winnerChecker = new WinnerChecker();
+            var result = winnerChecker.Game(player1, player2);
+
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [InlineData((int)t.scissors, (int)t.paper, "You win")]
+        [InlineData((int)t.scissors, (int)t.scissors, "Draw")]
+        [InlineData((int)t.scissors, (int)t.rock, "You lose")]
+        public void  CheckScissorsTestTheory(int player1, int player2, string expectedResult){
+            WinnerChecker winnerChecker = new WinnerChecker();
+            var result = winnerChecker.Game(player1, player2);
+
+            Assert.Equal(expectedResult, result);
         }
     }
 }
